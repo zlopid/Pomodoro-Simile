eaf7b8bc5aaffa81e938ffc380483178b16bc74e
Aaron Fetterman <zlopid@gmail.com>
2011-09-21 22:40:49 -0400

    Adding an alert when the model finishes and firing 'done' when the model finishes, rather than a second later.

--- pomodoro-app.js
+++ pomodoro-app.js
@@ -1,26 +1,31 @@
-$(function(){ // TODO: Do I have to wrap it in this on-ready call?
+$(function(){
 	
 	var Timer = Backbone.Model.extend({
 		defaults: {
-			minutes: 25,
-			seconds: 0
+			minutes: 0,
+			seconds: 5
 		},
 
 		start: function() {
-			var doMore = _.bind(this.go, this);
-			_.delay(doMore, 1000);
+			var doAgain = _.bind(this.tick, this);
+			_.delay(doAgain, 1000);
 		},
 
-		go: function() {
-			if ((this.get("minutes") == 0) && (this.get("seconds") == 0)) {
-				this.trigger("done");
-				return;
-			} else if (this.get("seconds") == 0) {
+		tick: function() {
+			// Update the time
+			if (this.get("seconds") == 0) {
 				this.set({minutes: this.get("minutes")-1, seconds: 59});
 			} else {
 				this.set({seconds: this.get("seconds")-1});
 			}
-			this.start();
+			
+			// Notify when done
+			if ((this.get("minutes") == 0) && (this.get("seconds") == 0)) {
+				this.trigger("done");
+				return;
+			} else {
+				this.start();
+			}
 		}
 	});
 	
@@ -32,14 +37,31 @@ $(function(){ // TODO: Do I have to wrap it in this on-ready call?
 			this.render();
 			this.model.bind('change', this.render, this);
 			this.model.start();
+			
+			new TimerAlert({model: this.model});
 		},
 		
 		render: function() {
 			var minutes = this.model.get("minutes");
 			var seconds = this.model.get("seconds");
 			$(this.el).html(minutes + ":" + ((seconds < 10) ? "0" : "") + seconds);
+		},
+		
+		done: function() {
+			alert("Time's up!");
 		}
 	});
+	
+	// TimerAlert requires that you pass in the model
+	var TimerAlert = Backbone.View.extend({
+		initialize: function() {
+			this.model.bind('done', this.done);
+		},
+		
+		done: function() {
+			alert("Time's up!");
+		}
+	})
 	
 	var Pomodoro = new TimerView();
 });
